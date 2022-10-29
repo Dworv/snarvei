fbb560493273b7d27d3dc43c9048cb94460d421d
Dworv <dwarvyt@gmail.com>
2022-10-29 11:18:35 -0700

    fix: correct quotes and commas

--- src/libinput/input_code_gen.py
+++ src/libinput/input_code_gen.py
@@ -73,7 +73,7 @@ with open("src/libinput/codes.rs", "w") as fs:
     for group, val in doc.items():
         array = f"    static ref {group}_DICT: HashMap<u16, &'static str> = HashMap::from(["
         for tup in val.items():
-            array += "\n        " + str(tup)
+            array += "\n        " + str(tup).replace("'", '"') + ","
         array += "    ]);\n"
         file += array
     file += "}"
